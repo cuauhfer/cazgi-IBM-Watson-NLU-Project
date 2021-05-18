5043cd3ee922fbef6ed42100724636aa1a912ab0
Cuauhtli Fernando <cuauh_fer_bh@hotmail.com>
2021-05-17 22:29:33 -0500

    Sentiment analize backend

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,25 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key
+        }),
+        serviceUrl: api_url
+    });
+    return naturalLanguageUnderstanding;
+}
+
+const app = new express();
 app.use(express.static('client'))
 
 const cors_app = require('cors');
@@ -11,20 +30,107 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
+    const url = req.query.url;
+    const analyzeParams = {
+        'url': url,
+        'features':{
+            'keywords': {
+                //'sentiment': true,
+                'emotion': true,
+                'limit': 1
+            }
+        }
+    };
+    const watson = getNLUInstance();
 
-    return res.send({"happy":"90","sad":"10"});
+    watson.analyze(analyzeParams).then(results => {
+        list = results.result.keywords;
+        let emotions = list.map((elem) => {
+            return elem.emotion;
+        });
+        return res.send(emotions[0]);
+    }).catch(err => {
+        console.log(err);
+    });
+    return "Algo ocurrio";
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    const url = req.query.url;
+    const analyzeParams = {
+        'url': url,
+        'features':{
+            'keywords': {
+                'sentiment': true,
+                //'emotion': true,
+                'limit': 1
+            }
+        }
+    };
+    const watson = getNLUInstance();
+
+    watson.analyze(analyzeParams).then(results => {
+        list = results.result.keywords;
+        let sentiments = list.map((elem) => {
+            return elem.sentiment;
+        });
+        return res.send(sentiments[0]);
+    }).catch(err => {
+        console.log(err);
+    });
+    return "Algo ocurrio";
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    const text = req.query.text;
+    const analyzeParams = {
+        'text': text,
+        'features':{
+            'keywords': {
+                //'sentiment': true,
+                'emotion': true,
+                'limit': 1
+            }
+        }
+    };
+    const watson = getNLUInstance();
+
+    watson.analyze(analyzeParams).then(results => {
+        list = results.result.keywords;
+        let emotions = list.map((elem) => {
+            return elem.emotion;
+        });
+        return res.send(emotions[0]);
+    }).catch(err => {
+        console.log(err);
+    });
+    return "Algo ocurrio";
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    const text = req.query.text;
+    const analyzeParams = {
+        'text': text,
+        'features':{
+            'keywords': {
+                'sentiment': true,
+                //'emotion': true,
+                'limit': 1
+            }
+        }
+    };
+    const watson = getNLUInstance();
+
+    watson.analyze(analyzeParams).then(results => {
+        list = results.result.keywords;
+        let sentiments = list.map((elem) => {
+            return elem.sentiment;
+        });
+        return res.send(sentiments[0]);
+    }).catch(err => {
+        console.log(err);
+    });
+    return "Algo ocurrio";
 });
 
 let server = app.listen(8080, () => {
